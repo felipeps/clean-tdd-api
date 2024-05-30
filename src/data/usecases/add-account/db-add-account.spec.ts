f6d51b628b6a6d0036a6b9a25978f549819b1274
Felipe Santos <nfelipe.ps@gmail.com>
2024-05-30 12:17:36 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -109,4 +109,22 @@ describe('DbAddAccount Usecase', () => {
 
     expect(sut.add(accountData)).rejects.toThrow()
   })
+
+  test('Should call an account on success', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    }
+
+    const account = await sut.add(accountData)
+
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'hashed_valid_password'
+    })
+  })
 })
