21a77e53fb4976a6f692eb790ef7f2d0a08cb13b
Felipe Santos <nfelipe.ps@gmail.com>
2024-05-30 23:15:19 -0300

    feat: ensure SignUpController calls Validation with correct value

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -2,20 +2,25 @@ import { AddAccount } from '../../../domain/usecases/add-account'
 import { InvalidParamError } from '../../errors/invalid-param-error'
 import { MissingParamError } from '../../errors/missing-param-error'
 import { badRequest, ok, serverError } from '../../helpers/http-helper'
+import { Validation } from '../../helpers/validators/validation'
 import { Controller } from '../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 
 export class SignUpController implements Controller {
   private readonly addAccount: AddAccount
+  private readonly validation: Validation
 
-  constructor (addAccount: AddAccount) {
+  constructor (addAccount: AddAccount, validation: Validation) {
     this.addAccount = addAccount
+    this.validation = validation
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-
     try {
+      this.validation.validate(httpRequest.body)
+
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field))
