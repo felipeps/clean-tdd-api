f3301ed4111f1178f34be52835b127e90e406ea4
Felipe Santos <nfelipe.ps@gmail.com>
2024-05-31 12:56:12 -0300

    feat: ensure RequiredFieldValidation does not returns if field is present

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -7,4 +7,10 @@ describe('RequiredFields Validation', () => {
     const error = sut.validate({ name: 'any_value' })
     expect(error).toEqual(new MissingParamError('field'))
   })
+
+  test('Should not return if validation succeeds', () => {
+    const sut = new RequiredFieldValidation('field')
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toBeFalsy()
+  })
 })
