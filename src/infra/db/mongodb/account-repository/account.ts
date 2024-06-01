b3116864234ccf96b7acca6d1559a8f591ba811f
Felipe Santos <nfelipe.ps@gmail.com>
2024-06-01 15:47:49 -0300

    feat: ensure loadByEmail returns an account if it exists

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,13 +1,32 @@
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne({ ...accountData })
     const account = { ...accountData, id: result.insertedId.toString() }
     return account
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({
+      email
+    })
+
+    if (!account) {
+      return null
+    }
+
+    return {
+      id: account._id.toString(),
+      name: account.name,
+      email: account.email,
+      password: account.password
+    }
   }
 }
