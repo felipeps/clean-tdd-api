e5b5d59cf3954909a828c24477165f20e43712ee
Felipe Santos <nfelipe.ps@gmail.com>
2024-06-01 16:42:09 -0300

    feat: ensure DbAddAccount calls loadByEmail with correct value

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -2,17 +2,22 @@ import { AccountModel } from '../../../domain/models/account'
 import { AddAccount, AddAccountModel } from '../../../domain/usecases/add-account'
 import { AddAccountRepository } from '../../protocols/db/add-account-repository'
 import { Hasher } from '../../protocols/cryptography/hasher'
+import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
 
 export class DbAddAccount implements AddAccount {
   private readonly hasher: Hasher
   private readonly addAccountRepository: AddAccountRepository
+  private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
 
-  constructor (hasher: Hasher, addAccountRepository: AddAccountRepository) {
+  constructor (hasher: Hasher, addAccountRepository: AddAccountRepository, loadAccountByEmailRepository: LoadAccountByEmailRepository) {
     this.hasher = hasher
     this.addAccountRepository = addAccountRepository
+    this.loadAccountByEmailRepository = loadAccountByEmailRepository
   }
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
+    this.loadAccountByEmailRepository.loadByEmail(accountData.email)
+
     const hashedPassword = await this.hasher.hash(accountData.password)
     const account = await this.addAccountRepository.add({
       ...accountData,
--- src/main/factories/signup/signup-factory.ts
+++ src/main/factories/signup/signup-factory.ts
@@ -1,4 +1,4 @@
-import { DbAddAccount } from '../../../data/usecases/add-account/db-account-account'
+import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../../infra/cryptography/bcrypt-adapter/bcrypt-adapter'
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
 import { SignUpController } from '../../../presentation/controllers/signup/signup'
@@ -7,7 +7,7 @@ import { makeSignUpValidation } from './signup-validation'
 export const makeSignUpController = (): SignUpController => {
   const accountRepository = new AccountMongoRepository()
   const hasher = new BcryptAdapter(12)
-  const dbAddAccount = new DbAddAccount(hasher, accountRepository)
+  const dbAddAccount = new DbAddAccount(hasher, accountRepository, accountRepository)
   const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation())
   return signUpController
 }
