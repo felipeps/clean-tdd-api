00ce11a6e632951d84ed1746f7b124c50bb323bf
Felipe Santos <nfelipe.ps@gmail.com>
2024-06-02 20:12:42 -0300

    feat: add create post factory

---
+++ src/main/factories/create-post/create-post-factory.ts
@@ -0,0 +1,11 @@
+import { PostMongoRepository } from '../../../infra/db/mongodb/post-repository/post'
+import { CreatePostController } from '../../../presentation/controllers/create-post/create-post'
+import { Controller } from '../../../presentation/protocols/controller'
+import { makeCreatePostValidation } from './create-post-validation'
+
+export const makeCreatePostController = (): Controller => {
+  const validations = makeCreatePostValidation()
+  const postRepository = new PostMongoRepository()
+  const postController = new CreatePostController(validations, postRepository)
+  return postController
+}
