eb3ae1a47e75d35177fa3aa5dbf93f047a0203ec
Felipe Santos <nfelipe.ps@gmail.com>
2024-06-02 20:16:52 -0300

    test: ensure /post returns 200 on success

---
+++ src/main/routes/post-routes.spec.ts
@@ -0,0 +1,32 @@
+import request from 'supertest'
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+
+describe('Post Routes', () => {
+  let postCollection: Collection
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  beforeEach(async () => {
+    postCollection = MongoHelper.getCollection('posts')
+    await postCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return 200 on create post', async () => {
+    await request(app)
+      .post('/api/post')
+      .send({
+        name: 'any_name',
+        content: 'any_content',
+        userId: 'any_user_id'
+      })
+      .expect(200)
+  })
+})
