47d9f58029565ed6dd9060f408ecd02ea3df3599
Felipe Santos <nfelipe.ps@gmail.com>
2024-06-02 20:16:48 -0300

    feat: ensure /post returns 200 on success

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,8 +1,10 @@
 import { Express, Router } from 'express'
 import signRoutes from '../routes/login-routes'
+import postRoutes from '../routes/post-routes'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
   signRoutes(router)
+  postRoutes(router)
 }
---
+++ src/main/routes/post-routes.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express'
+import { makeCreatePostController } from '../factories/create-post/create-post-factory'
+import { adaptRoute } from '../adapters/express-route-adapter'
+
+export default (router: Router): void => {
+  router.post('/post', adaptRoute(makeCreatePostController()))
+}
