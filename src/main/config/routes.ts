75e0b97a0d5489b204d44c4e60d07f01488b17d1
Felipe Santos <nfelipe.ps@gmail.com>
2024-06-02 20:58:05 -0300

    refactor: move login routes to a folder

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,10 +1,10 @@
 import { Express, Router } from 'express'
-import signRoutes from '../routes/login-routes'
+import loginRoutes from '../routes/login/login-routes'
 import postRoutes from '../routes/post-routes'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  signRoutes(router)
+  loginRoutes(router)
   postRoutes(router)
 }
