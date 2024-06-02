7a158ce6a56f2550b5b6937a437b19c486da70de
Felipe Santos <nfelipe.ps@gmail.com>
2024-06-02 19:37:11 -0300

    test: ensure CreatPostController returns an error if Validation returns an error

--- src/presentation/controllers/create-post/create-post.spec.ts
+++ src/presentation/controllers/create-post/create-post.spec.ts
@@ -1,3 +1,5 @@
+import { MissingParamError } from '../../errors/missing-param-error'
+import { badRequest } from '../../helpers/http-helper'
 import { Validation } from '../../helpers/validators/validation'
 import { Controller } from '../../protocols/controller'
 import { CreatePostController } from './create-post'
@@ -41,5 +43,22 @@ describe('CreatePost Controller', () => {
     await sut.handle(httpRequest)
 
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        content: 'any_email@mail.com',
+        userId: 'any_user_id'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
   })
 })
