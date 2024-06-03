2ce27a73a50226dc9b957391c664fdfc459efb53
Felipe Santos <nfelipe.ps@gmail.com>
2024-06-02 21:46:18 -0300

    feat: ensure GET /post returns 200

--- src/main/routes/post/post-routes.ts
+++ src/main/routes/post/post-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { makeCreatePostController } from '../../factories/create-post/create-post-factory'
 import { adaptRoute } from '../../adapters/express-route-adapter'
+import { makeLoadPostController } from '../../factories/load-post/load-post-factory'
 
 export default (router: Router): void => {
   router.post('/post', adaptRoute(makeCreatePostController()))
+  router.get('/post', adaptRoute(makeLoadPostController()))
 }
