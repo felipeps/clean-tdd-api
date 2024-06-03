763acda8b02d34ba8b0a7b5f653d06a8e6334e96
Felipe Santos <nfelipe.ps@gmail.com>
2024-06-02 21:41:55 -0300

    test: ensure PostMongoRepository return posts filtered by name

--- src/infra/db/mongodb/post-repository/post.spec.ts
+++ src/infra/db/mongodb/post-repository/post.spec.ts
@@ -18,17 +18,62 @@ describe('Post Mongo Repository', () => {
     await MongoHelper.disconnect()
   })
 
-  test('Should return a post on add success', async () => {
-    const sut = new PostMongoRepository()
-    const account = await sut.add({
-      name: 'any_name',
-      content: 'any_content',
-      userId: 'any_user_id'
+  describe('add()', () => {
+    test('Should return a post on add success', async () => {
+      const sut = new PostMongoRepository()
+      const post = await sut.add({
+        name: 'any_name',
+        content: 'any_content',
+        userId: 'any_user_id'
+      })
+      expect(post).toBeTruthy()
+      expect(post.id).toBeTruthy()
+      expect(post.name).toBe('any_name')
+      expect(post.content).toBe('any_content')
+      expect(post.userId).toBe('any_user_id')
     })
-    expect(account).toBeTruthy()
-    expect(account.id).toBeTruthy()
-    expect(account.name).toBe('any_name')
-    expect(account.content).toBe('any_content')
-    expect(account.userId).toBe('any_user_id')
+  })
+
+  describe('load()', () => {
+    test('Should return list of posts on load success filtering by name', async () => {
+      const sut = new PostMongoRepository()
+
+      await postCollection.insertMany([
+        {
+          name: 'any_name',
+          content: 'any_content',
+          userId: 'any_user_id'
+        },
+        {
+          name: 'any_name',
+          content: 'any_content_2',
+          userId: 'any_user_id_2'
+        },
+        {
+          name: 'any_name_3',
+          content: 'any_content_3',
+          userId: 'any_user_id_3'
+        }
+      ])
+
+      const posts = await sut.load({
+        name: 'any_name'
+      })
+
+      expect(posts).toEqual([
+        {
+          id: expect.any(String),
+          name: 'any_name',
+          content: 'any_content',
+          userId: 'any_user_id'
+        },
+        {
+          id: expect.any(String),
+          name: 'any_name',
+          content: 'any_content_2',
+          userId: 'any_user_id_2'
+        }
+      ])
+    })
   })
 })
