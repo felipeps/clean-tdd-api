279ec5bbbc745630812dd6d4e72eb1f133c3c4fa
Felipe Santos <nfelipe.ps@gmail.com>
2024-06-02 21:19:10 -0300

    test: ensure LoadPostController calls LoadPost with correct values

---
+++ src/presentation/controllers/load-post/load-post.spec.ts
@@ -0,0 +1,57 @@
+import { LoadPost } from '../../../domain/usecases/load-post'
+import { LoadPostController } from './load-post'
+
+const makeLoadPost = (): LoadPost => {
+  class LoadPostStub implements LoadPost {
+    async load (post: any): Promise<any> {
+      return [
+        {
+          id: 'valid_id',
+          name: 'any_name',
+          content: 'any_content'
+        },
+        {
+          id: 'valid_id_2',
+          name: 'any_name_2',
+          content: 'any_content_2'
+        }
+      ]
+    }
+  }
+  return new LoadPostStub()
+}
+
+interface SutTypes {
+  sut: LoadPostController
+  loadPostStub: LoadPost
+
+}
+
+const makeSut = (): SutTypes => {
+  const loadPostStub = makeLoadPost()
+  const sut = new LoadPostController(loadPostStub)
+  return {
+    sut,
+    loadPostStub
+  }
+}
+
+describe('LoadPost Controller', () => {
+  test('Should call LoadPost with correct values', async () => {
+    const { sut, loadPostStub } = makeSut()
+    const addSpy = jest.spyOn(loadPostStub, 'load')
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        userId: 'any_user_id'
+      }
+    }
+
+    await sut.handle(httpRequest)
+
+    expect(addSpy).toHaveBeenCalledWith({
+      name: 'any_name',
+      userId: 'any_user_id'
+    })
+  })
+})
