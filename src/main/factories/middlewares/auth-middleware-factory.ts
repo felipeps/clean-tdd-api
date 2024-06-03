e32ed4aa55505ca61338be93d76cd059ac2781a7
Felipe Santos <nfelipe.ps@gmail.com>
2024-06-02 22:18:50 -0300

    test: ensure POST /post returns 200 if auth

--- src/main/factories/middlewares/auth-middleware-factory.ts
+++ src/main/factories/middlewares/auth-middleware-factory.ts
@@ -1,9 +1,9 @@
-import { env } from 'process'
 import { DbLoadAccountByToken } from '../../../data/usecases/load-account-by-token/db-load-account-by-token'
 import { AuthMiddleware } from '../../../presentation/middlewares/auth-middleware'
 import { Middleware } from '../../../presentation/protocols/middleware'
 import { JwtAdapter } from '../../../infra/cryptography/jwt-adapter/jwt-adapter'
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
+import env from '../../config/env'
 
 export const makeAuthMiddleware = (): Middleware => {
   const jwtAdapter = new JwtAdapter(env.jwtSecret)
--- src/main/routes/post/post-routes.spec.ts
+++ src/main/routes/post/post-routes.spec.ts
@@ -1,10 +1,13 @@
 import request from 'supertest'
+import { sign } from 'jsonwebtoken'
 import { Collection } from 'mongodb'
 import { MongoHelper } from '../../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../../config/app'
+import env from '../../config/env'
 
 describe('Post Routes', () => {
   let postCollection: Collection
+  let accountCollection: Collection
 
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
@@ -13,6 +16,9 @@ describe('Post Routes', () => {
   beforeEach(async () => {
     postCollection = MongoHelper.getCollection('posts')
     await postCollection.deleteMany({})
+
+    accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   afterAll(async () => {
@@ -28,6 +34,33 @@ describe('Post Routes', () => {
         userId: 'any_user_id'
       })
       .expect(403)
+  })
+
+  test('Should return 200 on create post if valid auth', async () => {
+    const res = await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    const accessToken = sign({ id: res.insertedId.toString() }, env.jwtSecret)
+
+    await accountCollection.updateOne({
+      _id: res.insertedId
+    }, {
+      $set: {
+        accessToken
+      }
+    })
+
+    await request(app)
+      .post('/api/post')
+      .set('x-access-token', accessToken)
+      .send({
+        name: 'any_name',
+        content: 'any_content',
+        userId: 'any_user_id'
+      })
+      .expect(200)
   })
 
   test('Should return 200 on load post', async () => {
