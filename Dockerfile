430765742a7d9313c81b0fd58a0bac90d9d9aa67
Felipe Santos <nfelipe.ps@gmail.com>
2024-06-18 17:30:23 -0300

    chore: add docker-compose

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,3 @@ FROM node:18
 WORKDIR /usr/src/clean-tdd-api
 COPY ./package.json .
 RUN npm install --only=prod
-COPY ./dist ./dist
-EXPOSE 5000
-CMD npm start
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/rest-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/rest-api',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'jTsmh-351!'
 }
